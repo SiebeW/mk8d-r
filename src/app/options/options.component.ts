a078c53a5b0f7408eb15df499a53bae381e4ae68
SiebeW <siebe.wierdsma@gmail.com>
2022-08-12 18:35:27 +0200

    prep for update

--- src/app/options/options.component.ts
+++ src/app/options/options.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import MarioKartJson from '../../assets/MarioKartIconsAndData.json';
+import { MarioKartData } from '../models/marioKartData.model';
 
 @Component({
   selector: 'app-options',
@@ -6,14 +8,31 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./options.component.scss']
 })
 export class OptionsComponent implements OnInit {
+  private data: MarioKartData = MarioKartJson;
+  public miiImage = this.getMiiImageUrl()
 
   constructor() { }
 
   ngOnInit(): void {
-    
+
   }
   ngOnDestroy(): void {
 
+  }
+
+  private getMiiImageUrl() {
+    let mii = {
+      Name: '',
+      HasAlternateColor: false,
+      ImageURL: ''
+    };
+    this.data.Characters.find(c => {
+      console.log(c);
+      if (c.Name == "Mii") {
+        mii = c;
+      }
+    })
+    return mii.ImageURL;
   }
 
 }
