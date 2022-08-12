a4a082a2af55e41a72c6bf076cca2dd2c4fbf1c8
Siebe Wierdsma <siebe@Siebes-iMac.lan>
2022-08-12 11:24:51 +0200

    initial commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,32 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
-  title = 'mk8d-r';
+export class AppComponent implements OnInit {
+  private date = Date.now();
+  public now = this.getDateTime();
+
+  constructor() { }
+
+  ngOnInit(): void {}
+
+  private getDateTime():string {
+    let milliseconds = parseInt('' + (this.date%1000)/100);
+    let nSeconds = parseInt('' + (this.date/1000)%60);
+    let nMinutes = parseInt('' + (this.date/(1000*60))%60);
+    let nHours = parseInt('' + (this.date/(1000*60*60))%24);
+    let hours = (nHours < 10) ? "0" + nHours : nHours;
+    let minutes = (nMinutes < 10) ? "0" + nMinutes : nMinutes;
+    let seconds = (nSeconds < 10) ? "0" + nSeconds : nSeconds;
+
+    let today = new Date();
+    let dd = String(today.getDate()).padStart(2,'0');
+    let mm = String(today.getMonth() + 1).padStart(2,'0');
+    let yyyy = today.getFullYear();
+
+    return dd + '/' + mm + '/' + yyyy + ' at ' + hours + ":" + minutes + ":" + seconds + "." + milliseconds;
+  }
 }
