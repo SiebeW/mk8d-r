4160154c224a9b76b3c0dcfcef22cab7f21dd49a
SiebeW <siebe.wierdsma@gmail.com>
2023-11-16 23:26:40 +0100

    minor fix on generate selection button

--- src/app/models/marioKartData.model.ts
+++ src/app/models/marioKartData.model.ts
@@ -1,27 +1,25 @@
-  import { Base } from "../models/base.model.js";
+import { Base } from "../models/base.model.js";
 
+interface IVehiclePart {
+  name: string;
+  imageURL: string;
+  type?: string;
+}
 
-  interface IVehiclePart {
-    name: string;
-    imageURL: string;
-    type?: string;
+interface Vehicles {
+  bodies: IVehiclePart[];
+  tires: IVehiclePart[];
+  gliders: IVehiclePart[];
+}
+
+export class MarioKartData {
+  public characters: Base[] = [];
+  public vehicles: Vehicles = { bodies: [], tires: [], gliders: [] };
+
+  constructor(characters: Base[], vehicles: Vehicles) {
+    Object.assign(this, characters, vehicles);
   }
-
-  interface Vehicles {
-    bodies: IVehiclePart[];
-    tires: IVehiclePart[];
-    gliders: IVehiclePart[];
-  }
-  
-  export class MarioKartData {
-    public characters: Base[] = [];
-    public vehicles: Vehicles = {bodies: [], tires: [], gliders: []};
-
-    constructor(characters: Base[], vehicles: Vehicles) {
-      Object.assign(this, characters, vehicles);
-    }
-  }
-
+}
 
 export class VehiclePart {
   public name: string = '';
@@ -29,7 +27,6 @@ export class VehiclePart {
   public type: string = '';
 
   constructor(vehiclePart: VehiclePart) {
-      Object.assign(this, vehiclePart);
-    }
-  
+    Object.assign(this, vehiclePart);
+  }
 }
