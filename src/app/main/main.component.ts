cd5a47c5f1eb98a8a433356acb33fdc04d930375
SiebeW <siebe.wierdsma@gmail.com>
2023-11-17 15:20:37 +0100

    1337

--- src/app/main/main.component.ts
+++ src/app/main/main.component.ts
@@ -10,17 +10,31 @@ import { Selection } from '../models/selection.model';
 })
 export class MainComponent implements OnInit {
   private unsubscribeSubject: Subject<boolean> = new Subject<boolean>;
-  private dateObj = new Date;
-  public now = this.dateObj.toLocaleDateString() + " at " + this.dateObj.toLocaleTimeString();
-  
+  public dateObj = new Date;
+  public hoursminutes = this.formatHoursMinutes();
+
   constructor(private optionsService: OptionsService) { }
 
   ngOnInit(): void {
-      this.optionsService.selectionsArray.pipe(takeUntil(this.unsubscribeSubject)).subscribe((sel: Selection[]) => {
-        if (sel.length === 4) {
-          this.dateObj = new Date;
-          this.now = this.dateObj.toLocaleDateString() + " at " + this.dateObj.toLocaleTimeString();
-        }
-      });
+    this.optionsService.selectionsArray.pipe(takeUntil(this.unsubscribeSubject)).subscribe((sel: Selection[]) => {
+      if (sel.length === 4) {
+        this.dateObj = new Date;
+        this.hoursminutes = this.formatHoursMinutes();
+      }
+    });
+  }
+  public checkFor1337(): string {
+    if (this.hoursminutes === '13:37') {
+      return 'elite-gold';
     }
+    return '';
+  }
+  public formatNumberIfShort(x: number): string {
+    console.log(x + ' is this many characters long: ' + x.toString().length);
+    const fixedNumber = x.toString().length === 1 ? '0'.concat(x.toString()) : x.toString();
+    return fixedNumber;
+  }
+  private formatHoursMinutes(): string {
+    return this.formatNumberIfShort(this.dateObj.getHours()) + ':' + this.formatNumberIfShort(this.dateObj.getMinutes());
+  }
 }
